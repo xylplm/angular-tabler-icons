157caf30f1ad5f5738489ac3da30c48208015d33
xylplm <xylplm@users.noreply.github.com>
2026-03-19 08:20:53 +0000

    Tabler Icons v3.40.0

---
+++ projects/angular-tabler-icons/icons/svg/circle-open-arrow-down.ts
@@ -0,0 +1,1 @@
+export const IconCircleOpenArrowDown = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" >  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>  <path d="M15.998 3.934a9 9 0 1 1 -3.998 -.934v13" />  <path d="M16 12l-4 4l-4 -4" /></svg>`
