1d0016cadbcc21e44efd62f063d682b916b8d716
xylplm <xylplm@users.noreply.github.com>
2026-03-19 08:15:52 +0000

    Tabler Icons v3.38.0

---
+++ projects/angular-tabler-icons/icons/svg/chevron-right-filled.ts
@@ -0,0 +1,1 @@
+export const IconChevronRightFilled = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor">  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>  <path d="M9.707 5.293l6 6a1 1 0 0 1 0 1.414l-6 6a1 1 0 1 1 -1.414 -1.414l5.293 -5.293l-5.293 -5.293a1 1 0 0 1 1.414 -1.414" /></svg>`
---
+++ projects/angular-tabler-icons/icons/svg/plus-filled.ts
@@ -0,0 +1,1 @@
+export const IconPlusFilled = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor">  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>  <path d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 0 1 0 2h-6v6a1 1 0 0 1 -2 0v-6h-6a1 1 0 0 1 0 -2h6v-6a1 1 0 0 1 1 -1" /></svg>`
